8be9e7ced23cb0929e8b62681b1ed52ab8577138
Joost-dm <vasiliishirokov@gmail.com>
2020-06-03 01:53:35 +0300

    commit

--- jooster/backend/manage.py
+++ jooster/backend/manage.py
@@ -4,7 +4,6 @@ import os
 import sys
 
 def main():
-    print('test')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
     try:
         from django.core.management import execute_from_command_line
