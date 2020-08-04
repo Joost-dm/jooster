8163a51261128477e973dc21e3e0a2e6b040a806
Joost-dm <vasiliishirokov@gmail.com>
2020-08-04 21:58:13 +0300

    commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -42,6 +42,37 @@ export default {
   methods: {
     closeError () {
       this.$store.dispatch('clearError')
+    },
+    mobileScreenHeightController () {
+      alert('work')
+      var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+      var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+      var isPortrait = viewportHeight > viewportWidth
+
+      window.addEventListener('resize', onresize)
+
+      function onresize () {
+        var newViewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+        var newViewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+        var hasOrientationChanged = (newViewportHeight > newViewportWidth) !== isPortrait
+        var addressbarHeight = 130
+
+        if (!hasOrientationChanged && (newViewportHeight !== viewportHeight)) {
+          addressbarHeight = Math.abs(newViewportHeight - viewportHeight)
+          alert(addressbarHeight)
+          if (newViewportHeight < viewportHeight) {
+            // Android Chrome address bar has appeared
+          } else {
+            // Android Chrome address bar has disappeared
+          }
+        } else if (hasOrientationChanged) {
+          // Orientation change
+        }
+
+        viewportHeight = newViewportHeight
+        viewportWidth = newViewportWidth
+        isPortrait = viewportHeight > viewportWidth
+      }
     }
   },
   computed: {
@@ -59,6 +90,7 @@ export default {
     }
   },
   created () {
+    this.mobileScreenHeightController()
     this.$store.dispatch('checkForLocalAuthToken')
   }
 }
