296eb715d9ac2725f05fa89210c335903da5f8db
Joost-dm <vasiliishirokov@gmail.com>
2020-08-05 00:59:29 +0300

    commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -44,11 +44,12 @@ export default {
       this.$store.dispatch('clearError')
     },
     mobileScreenHeightController () {
-      window.addEventListener('resize', onresize)
-
-      function onresize () {
-        this.$store.dispatch('mobileScreenHeightController')
-      }
+      var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+      var appBarHeight = document.getElementById('v-app-bar').offsetHeight
+      var headerHeight = document.getElementsByClassName('primary-view__header')[0].offsetHeight
+      var bottomFormHeight = document.getElementsByClassName('primary-view__bottom-form')[0].offsetHeight
+      var primaryBody = document.getElementsByClassName('primary-view__body')[0]
+      primaryBody.style.height = (viewportHeight - appBarHeight - headerHeight - bottomFormHeight + 12) + 'px'
     }
   },
   computed: {
@@ -69,7 +70,7 @@ export default {
     this.$store.dispatch('checkForLocalAuthToken')
   },
   mounted () {
-    this.mobileScreenHeightController()
+    window.addEventListener('resize', this.mobileScreenHeightController)
   }
 }
 </script>
