80d5ca4e4080b1f56792ffd3bd6ba820b9708c90
Joost-dm <vasiliishirokov@gmail.com>
2020-08-07 00:46:58 +0300

    commit

--- main/settings.py
+++ main/settings.py
@@ -184,11 +184,14 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+"""
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
