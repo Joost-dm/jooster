35c207ccb615559a681a6144c7c8810df6be534b
Joost-dm <vasiliishirokov@gmail.com>
2020-08-23 23:11:19 +0300

    commit

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -1,6 +1,8 @@
 <template>
-
   <div class="login">
+    <div class="login__animation-bg1"></div>
+    <div class="login__animation-bg1 login__animation-bg2"></div>
+    <div class="login__animation-bg1 login__animation-bg3"></div>
     <div class="login__content">
       <div class="login__header">
         <div class="login__header-title">
@@ -74,6 +76,7 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 5%;
 }
 .login__content {
   width: 100%;
@@ -81,9 +84,10 @@ export default {
   min-width: 300px;
   overflow: hidden;
   border-radius: 10px 10px 0 0;
+  z-index: 1;
 }
 .login__header {
-  height: 60px;
+  height:45px;
   width: 100%;
   background-color: $primary-dark;
   display: flex;
@@ -96,6 +100,35 @@ export default {
 .login__form {
   background-color: $secondary;
   padding: 1rem;
+}
+.login__animation-bg1 {
+  animation:slide 3s ease-in-out infinite alternate;
+  background-image: linear-gradient(-60deg, $primary-dark, 50%, $secondary 50%);
+  bottom:0;
+  left:-50%;
+  opacity:.5;
+  position:fixed;
+  right:-50%;
+  top:0;
+  z-index:0;
+}
+
+.login__animation-bg2 {
+  animation-direction:alternate-reverse;
+  animation-duration:4s;
+}
+
+.login__animation-bg3 {
+  animation-duration:5s;
+}
+
+@keyframes slide {
+  0% {
+    transform:translateX(-25%);
+  }
+  100% {
+    transform:translateX(25%);
+  }
 }
 
 </style>
